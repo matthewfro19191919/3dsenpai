7985763803d44fed29e526c4b535ae0b220a1425
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-02-14 05:46:27 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,21 +1,18 @@
 package;
 
-import modding.PolymodHandler;
+import openfl.net.SharedObject;
+import haxe.Unserializer;
+import sys.io.File;
+import sys.FileSystem;
+import openfl.system.System;
 import flixel.system.scaleModes.RatioScaleMode;
-import extensions.flixel.FlxUIStateExt;
-import transition.data.*;
 import flixel.FlxG;
 import flixel.FlxGame;
-import openfl.display.Sprite;
-import debug.*;
 import openfl.display.FPS;
-#if mobile
-import mobile.MobileUtil;
-#end
+import openfl.display.Sprite;
 
 class Main extends Sprite
 {
-
 	public static var fpsDisplay:FPS_Mem;
 
 	public static var novid:Bool = false;
@@ -25,54 +22,44 @@ class Main extends Sprite
 
 	public function new()
 	{
-		#if mobile
-		#if android
-		MobileUtil.requestPermsFromUser();
-		#end
-		Sys.setCwd(MobileUtil.getStorageDirectory());
-		#end
-		CrashHandler.init();
-
 		super();
 
-		PolymodHandler.init();
-
-		#if (sys && !mobile)
+		#if sys
 		novid = Sys.args().contains("-novid");
 		flippymode = Sys.args().contains("-flippymode");
 		#end
 
-		SaveManager.global();
+		addChild(new FlxGame(0, 0, Startup, 1, 144, 144, true));
 
-		fpsDisplay = new FPSExt(3, 3, 0xFFFFFF);
+		#if !mobile
+		fpsDisplay = new FPS_Mem(10, 3, 0xFFFFFF);
 		fpsDisplay.visible = true;
-
-		FlxUIStateExt.defaultTransIn = ScreenWipeIn;
-		FlxUIStateExt.defaultTransInArgs = [0.6];
-                FlxUIStateExt.defaultTransOut = ScreenWipeOut;
-                FlxUIStateExt.defaultTransOutArgs = [0.6];
-
-		addChild(new FlxGame(#if mobile 1280, 720 #else 0, 0 #end, #if mobile !mobile.CopyState.checkExistingFiles() ? mobile.CopyState : #end Startup, 60, 60, true));
 		addChild(fpsDisplay);
+		switch (FlxG.save.data.fpsDisplayValue)
+		{
+			case 0:
+				Main.fpsDisplay.visible = true;
+				Main.fpsDisplay.showMem = true;
+			case 1:
+				Main.fpsDisplay.visible = true;
+				Main.fpsDisplay.showMem = false;
+			case 2:
+				Main.fpsDisplay.visible = false;
+		}
+		#end
 
-		//On web builds, video tends to lag quite a bit, so this just helps it run a bit faster.
+		FlxG.signals.postStateSwitch.add(function()
+		{
+			System.gc();
+		});
+
+		// On web builds, video tends to lag quite a bit, so this just helps it run a bit faster.
 		#if web
 		VideoHandler.MAX_FPS = 30;
 		#end
 
-		#if mobile
-		#if android FlxG.android.preventDefaultKeys = [BACK]; #end
-
-		FlxG.signals.gameResized.add(function (w, h) {
-			if(fpsDisplay != null)
-				fpsDisplay.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
-		});
-		#end
-
-		#if (sys && !mobile)
 		trace("-=Args=-");
 		trace("novid: " + novid);
 		trace("flippymode: " + flippymode);
-		#end
 	}
 }
